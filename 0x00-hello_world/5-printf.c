016207c2dfa9112f2742d50204a3cd1b7c43755a
biruk-kinfe <kinfebiruk3@gmail.com>
2021-06-17 12:59:30 -0700

    correctiong output

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
-#include <studio.h>
+#include <stdio.h>
+
+/**
+ * main - Prints quote
+ *
+ * Return: zero on success
+ *
+ */
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n ");
-  return(0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
