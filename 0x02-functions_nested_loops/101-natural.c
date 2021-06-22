79b2c403fbd40e8efff16bb06c95f5386dfd55e1
biruk-kinfe <kinfebiruk3@gmail.com>
2021-06-22 13:56:27 -0700

    fixing

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -4,7 +4,7 @@
  * main - print sum of all multiples of 3 and 5 up to 1024
  *
  * @void: Empty parameter list for main
-
+ *
  * Description: prints the sum of all multiples of 3 and 5
  * up to but not including 1024
  *
@@ -17,7 +17,7 @@ int sum, i;
 sum = 0;
 for (i = 1; i < 1024; i++)
 {
-if ((i % 3 ==0) || (i % 5 == 0))
+if ((i % 3 == 0) || (i % 5 == 0))
 sum += i;
 }
 printf("%d\n", sum);
