a2bd865cbeaf827eab0243d5299d8f331ff73fe1
biruk-kinfe <kinfebiruk3@gmail.com>
2021-06-27 04:51:20 -0700

    debugging

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,14 +10,14 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0))
+  if (year % 4 == 0 || ((year % 100 == 0) && (year % 400 == 0)))
 {
 if (month > 2 && day >= 60)
 {
 day++;
 }
 printf("Day of the year: %d\n", day);
-printf("Remaing days: %d\n", 366 - day);
+printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
@@ -28,7 +28,7 @@ printf("invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 else
 {
 printf("Day of the year: %d\n", day);
-printf("Reaming days: %d\n", 365 - day);
+printf("Remaining days: %d\n", 365 - day);
 }
 }
 }
